372d8db10588751dde7fe1c9e2d2300318cf0953
root <root@DESKTOP-4JHBKQS.>
2025-03-21 21:35:33 +0800

    update

--- exercises/strings/strings1.rs
+++ exercises/strings/strings1.rs
@@ -1,11 +1,4 @@
-// strings1.rs
-//
-// Make me compile without changing the function signature!
-//
-// Execute `rustlings hint strings1` or use the `hint` watch subcommand for a
-// hint.
-
-// I AM NOT DONE
+//DONE
 
 fn main() {
     let answer = current_favorite_color();
@@ -13,5 +6,5 @@ fn main() {
 }
 
 fn current_favorite_color() -> String {
-    "blue"
+    "blue".to_string()
 }
