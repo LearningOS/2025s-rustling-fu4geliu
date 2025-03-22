ae974cd028a4066e94303642afb3ce1217d6d1e3
root <root@DESKTOP-4JHBKQS.>
2025-03-22 16:35:21 +0800

    update

--- exercises/error_handling/errors1.rs
+++ exercises/error_handling/errors1.rs
@@ -13,8 +13,8 @@
 
 pub fn generate_nametag_text(name: String) -> Option<String> {
     if name.is_empty() {
-        // Empty names aren't allowed.
-        None
+        error!("`name` was empty; it must be nonempty.");
+        None 
     } else {
         Some(format!("Hi! My name is {}", name))
     }
