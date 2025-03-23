697533af5048f94f8fa64de749f7a6dd1384e58b
root <root@DESKTOP-4JHBKQS.>
2025-03-23 21:44:20 +0800

    update

--- exercises/error_handling/errors3.rs
+++ exercises/error_handling/errors3.rs
@@ -11,11 +11,11 @@
 
 use std::num::ParseIntError;
 
-fn main() {
+fn main(){
     let mut tokens = 100;
     let pretend_user_input = "8";
 
-    let cost = total_cost(pretend_user_input)?;
+    let cost = total_cost(pretend_user_input).expect("Failed to parse input.");
 
     if cost > tokens {
         println!("You can't afford that many!");
