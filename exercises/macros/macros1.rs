18fd8001304df481230d13d245c846f17889cfb0
root <root@DESKTOP-4JHBKQS.>
2025-03-26 22:58:51 +0800

    update

--- exercises/macros/macros1.rs
+++ exercises/macros/macros1.rs
@@ -1,9 +1,4 @@
-// macros1.rs
-//
-// Execute `rustlings hint macros1` or use the `hint` watch subcommand for a
-// hint.
-
-// I AM NOT DONE
+//DONE
 
 macro_rules! my_macro {
     () => {
@@ -12,5 +7,5 @@ macro_rules! my_macro {
 }
 
 fn main() {
-    my_macro();
+    my_macro!();
 }
