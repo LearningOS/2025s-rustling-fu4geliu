d6fdf779f01a094174735c6a2af0b78239b02bc8
root <root@DESKTOP-4JHBKQS.>
2025-03-31 23:03:52 +0800

    update

--- exercises/algorithm/algorithm4.rs
+++ exercises/algorithm/algorithm4.rs
@@ -51,12 +51,24 @@ where
     // Insert a value into the BST
     fn insert(&mut self, value: T) {
         //TODO
+        match self.root  {
+            Some(ref mut node) => node.insert(value), 
+            None => self.root  = Some(Box::new(TreeNode::new(value))),
+        }
     }
 
     // Search for a value in the BST
     fn search(&self, value: T) -> bool {
         //TODO
-        true
+        let mut current = &self.root; 
+        while let Some(node) = current {
+            match value.cmp(&node.value)  {
+                Ordering::Less => current = &node.left, 
+                Ordering::Greater => current = &node.right, 
+                Ordering::Equal => return true,
+            }
+        }
+        false
     }
 }
 
@@ -67,6 +79,23 @@ where
     // Insert a node into the tree
     fn insert(&mut self, value: T) {
         //TODO
+        match value.cmp(&self.value){
+            Ordering::Less => {
+                if let Some(left) = &mut self.left  {
+                    left.insert(value); 
+                } else {
+                    self.left  = Some(Box::new(TreeNode::new(value)));
+                }
+            }
+            Ordering::Greater => {
+                if let Some(right) = &mut self.right  {
+                    right.insert(value); 
+                } else {
+                    self.right  = Some(Box::new(TreeNode::new(value)));
+                }
+            }
+            Ordering::Equal => {} 
+        }
     }
 }
 
