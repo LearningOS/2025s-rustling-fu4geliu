b3a5b853880e25b3ad5a124c900bdc9304046293
root <root@DESKTOP-4JHBKQS.>
2025-04-01 22:25:25 +0800

    update

--- exercises/algorithm/algorithm8.rs
+++ exercises/algorithm/algorithm8.rs
@@ -1,8 +1,4 @@
-/*
-	queue
-	This question requires you to use queues to implement the functionality of the stac
-*/
-// I AM NOT DONE
+//DONE
 
 #[derive(Debug)]
 pub struct Queue<T> {
@@ -68,14 +64,24 @@ impl<T> myStack<T> {
     }
     pub fn push(&mut self, elem: T) {
         //TODO
+        self.q1.enqueue(elem);
     }
     pub fn pop(&mut self) -> Result<T, &str> {
         //TODO
-		Err("Stack is empty")
+        if !self.q1.elements.is_empty() {
+            while self.q1.size() > 1 {
+                self.q2.enqueue(self.q1.dequeue().unwrap());
+            }
+            std::mem::swap(&mut self.q1, &mut self.q2);
+            return self.q2.dequeue()
+        } else {
+            Err("Stack is empty")
+        }
     }
     pub fn is_empty(&self) -> bool {
 		//TODO
-        true
+        if self.q1.is_empty() { return true}
+        false
     }
 }
 
